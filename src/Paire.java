ff09002f994af4e73453ac3225bec50ac2575660
fabrice-bazire <bazire.fabrice@gmail.com>
2018-02-21 14:05:41 +0100

    final first commit

---
+++ src/Paire.java
@@ -0,0 +1,65 @@
+
+import java.util.*;
+public class Paire extends Liste {
+	   static Scanner in = new Scanner (System.in);
+
+	    public int x, y;
+
+	    public Paire(int x, int y){
+	        this.x = x;
+	        this.y = y;
+	    }
+
+	    public Paire(){
+	        System.out.print("Entrez x : ");
+	        int x = in.nextInt();
+	        this.x = x;
+	        System.out.print("Entrez y : ");
+	        int y = in.nextInt();
+	        this.y = y;
+	    }
+
+	    @Override
+	    public boolean equals(Object o) {
+	        if (this == o) return true;
+	        if (o == null || getClass() != o.getClass()) return false;
+	        Paire paire = (Paire) o;
+	        return x == paire.x && y == paire.y;
+	    }
+
+	    @Override
+	    public int hashCode() {
+
+	        return Objects.hash(x, y);
+	    }
+
+	    public int compare (Paire p){
+	        if (this.getx() < p.getx()){
+                return -1;
+            }
+            if (this.getx() > p.getx()){
+                return 1;
+            }
+            if (this.getx() == p.getx()){
+                if (this.gety() < p.gety()){
+                    return -1;
+                }
+                if (this.gety() > p.gety()){
+                    return 1;
+                }
+            }
+	        return 0;
+        }
+
+	    public int getx(){
+	        return this.x;
+	    }
+
+	    public int gety(){
+	        return this.y;
+	    }
+
+	    public String toString(){
+	        return ("(" + x + ";" + y +")");
+	    }
+}
---
+++ src/Testjeudelaviedebase.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Testjeudelaviedebase {
+
+    public static void main (String[]args) {
+        Liste listededepart = new Liste();
+        listededepart.addLast(new Paire(8, 3));
+        listededepart.addLast(new Paire(9, 3));
+        listededepart.addLast(new Paire(6, 3));
+        listededepart.addLast(new Paire(5, 3));
+        listededepart.addLast(new Paire(4, 3));
+        listededepart.addLast(new Paire(3, 3));
+        listededepart.addLast(new Paire(2, 3));
+        System.out.print(listededepart.toString());
+        System.out.println(" ");
+        System.out.println(" ");
+        listededepart.triliste();
+        System.out.print(listededepart.toString());
+    }
+}
