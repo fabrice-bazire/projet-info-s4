1f376c184ef7675246545458a9ff1143bf243ad4
fabrice-bazire <bazire.fabrice@gmail.com>
2018-03-21 13:57:48 +0100

    m

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 
-public class Main {
+/*public class Main {
 
     public static void main(String[] args) {
         Liste a = new Liste();
@@ -23,4 +23,4 @@ public class Main {
         System.out.println(finale.toString());
     }
 
-}
+}*/
--- src/Testjeudelaviedebase.java
+++ src/Testjeudelaviedebase.java
@@ -4,16 +4,9 @@ public class Testjeudelaviedebase {
     public static void main (String[]args) {
         Liste listededepart = new Liste();
         listededepart.addInHead(new Paire(14, 4));
-        listededepart.addInHead(new Paire(8, 7));
-        listededepart.addInHead(new Paire(6, 3));
-        listededepart.addInHead(new Paire(66, 43));
-        listededepart.addInHead(new Paire(6, 5));
-        listededepart.addInHead(new Paire(6, 43));
-        listededepart.addInHead(new Paire(6, 32));
-        listededepart.addInHead(new Paire(6, 13));
-        listededepart.addInHead(new Paire(6, 39));
-        listededepart.addInHead(new Paire(6, 35));
+        listededepart.addTrier(new Paire(8, 7));
+        listededepart.addTrier(new Paire(6, 3));
+        listededepart.addTrier(new Paire(6, 35));
         System.out.print(listededepart.toString());
-        System.out.print(listededepart.comportementasymptotique());
     }
 }
