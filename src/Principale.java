7c7b5abd479cbd4da656d52b89cc529485d28c14
fabrice <bazire.fabrice@gmail.com>
2018-04-19 10:03:35 +0200

    affichage de la liste en grille

--- src/Principale.java
+++ src/Principale.java
@@ -1,4 +1,36 @@
 public class Principale {
+
+    public static char [][] grille (Liste l){
+        char  grille [][] = new char [10][10];
+        for (int i = 0; i<grille.length; i++){
+            for (int j = 0; j<grille[1].length; j++){
+                grille[i][j] = ' ';
+            }
+        }
+        Maillon<Paire> ref = l.tete;
+        while (ref != null){
+            grille[ref.getValeur().x-1][ref.getValeur().y-1] = 'X';
+            ref = ref.getSuivant();
+        }
+        return grille;
+    }
+
+    public static void aff_grille (char [][] grille){
+        System.out.println("     1   2   3   4   5   6   7   8   9   10");
+        for (int i = 0; i<grille.length; i++) {
+            if (i < 9){
+                System.out.print(i+1 + " ");
+            }else{
+                System.out.print(i+1);
+            }
+
+            for (int j = 0; j < grille[1].length; j++) {
+                System.out.print(" | " + grille[i][j]);
+            }
+            System.out.println(" |");
+        }
+    }
+
     public static void main (String [] args){
        Rules a = new Rules();
        Liste dep = new Liste();
@@ -6,6 +38,9 @@ public class Principale {
        dep.addTrier(new Paire(2,3,0));
        dep.addTrier(new Paire(3,3,0));
        a.liste = dep;
-       System.out.print(dep);
+       //System.out.print(dep);
+       aff_grille(grille(a.liste));
+       a.newgeneration();
+       aff_grille(grille(a.liste));
     }
 }
