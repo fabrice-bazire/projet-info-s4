bbfdd4d7c862762f1b24575ced653683fe0dd7d0
fabrice <bazire.fabrice@gmail.com>
2018-05-03 11:37:06 +0200

    c

--- src/Maillon.java
+++ src/Maillon.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package Projet;
 
 /**
@@ -18,21 +19,33 @@ public class Maillon<T extends Comparable> implements Comparable<Maillon>{
     /**
      * Constructeur Projet.Maillon
      */
+=======
+public class Maillon<T extends Comparable> implements Comparable<Maillon>{
+
+
+    private T valeur;
+    private Maillon<T> suivant;
+
+>>>>>>> parent of a4c55b4... javadoc quasi complete + taille de la queue fonctionne + -s et -c fonctionne
      public Maillon (){
         this.valeur = null;
         this.suivant = null;
      }
 
+<<<<<<< HEAD
     /**
      * Constructeut Projet.Maillon qui initialise la valeur du maillon.
      * @param p
      * Un type T.
      */
+=======
+>>>>>>> parent of a4c55b4... javadoc quasi complete + taille de la queue fonctionne + -s et -c fonctionne
     public Maillon (T p) {
         this.valeur=p;
         this.suivant=null;
 
     }
+<<<<<<< HEAD
 
     /**
      * Constructeur Projet.Maillon.
@@ -41,45 +54,58 @@ public class Maillon<T extends Comparable> implements Comparable<Maillon>{
      * @param m
      * Un Projet.Maillon
      */
+=======
+>>>>>>> parent of a4c55b4... javadoc quasi complete + taille de la queue fonctionne + -s et -c fonctionne
     public Maillon(T p, Maillon<T > m) {
         valeur = p;
         suivant = m;
     }
 
+<<<<<<< HEAD
     /**
      * Retourne la valeur du Projet.Maillon.
      * @return la valeur du Projet.Maillon qui est de type T.
      */
+=======
+>>>>>>> parent of a4c55b4... javadoc quasi complete + taille de la queue fonctionne + -s et -c fonctionne
     public T getValeur() {
         return valeur;
     }
 
+<<<<<<< HEAD
     /**
      * Met à jour la valeur du Projet.Maillon.
      * @param valeur
      * Un type T.
      */
+=======
+>>>>>>> parent of a4c55b4... javadoc quasi complete + taille de la queue fonctionne + -s et -c fonctionne
     public void setValeur(T valeur) {
         this.valeur = valeur;
     }
 
+<<<<<<< HEAD
     /**
      * Retourne le suivant du Projet.Maillon this.
      * @return un Projet.Maillon qui est le suivant du Projet.Maillon this.
      */
+=======
+>>>>>>> parent of a4c55b4... javadoc quasi complete + taille de la queue fonctionne + -s et -c fonctionne
     public Maillon<T> getSuivant() {
         return suivant;
     }
 
+<<<<<<< HEAD
     /**
      * Met à jour le suivant du Projet.Maillon this.
      * @param suivant
      * Un maillon de type T.
      */
+=======
+>>>>>>> parent of a4c55b4... javadoc quasi complete + taille de la queue fonctionne + -s et -c fonctionne
     public void setSuivant(Maillon<T> suivant) {
         this.suivant = suivant;
     }
-
 
     @Override
     public int hashCode() {
@@ -89,12 +115,16 @@ public class Maillon<T extends Comparable> implements Comparable<Maillon>{
         return result;
     }
 
+<<<<<<< HEAD
     /**
      * Compare un objet avec le Projet.Maillon this.
      * @param obj
      * Un objet o.
      * @return un booléen vrai si l'objet est identique au Projet.Maillon this et faux sinon.
      */
+=======
+
+>>>>>>> parent of a4c55b4... javadoc quasi complete + taille de la queue fonctionne + -s et -c fonctionne
     @Override
     public boolean equals(Object obj) {
         if (this == obj)
@@ -112,12 +142,16 @@ public class Maillon<T extends Comparable> implements Comparable<Maillon>{
         return true;
     }
 
+<<<<<<< HEAD
     /**
      * Compare la valeur d'un Projet.Maillon avec le Projet.Maillon this.
      * @param o
      * Un maillon.
      * @return un entier, 1 si la valeur de this est plus grande que la valeur du Projet.Maillon passée en paramètre, -1 pour l'inverse et 0 si ils sont identiques.
      */
+=======
+
+>>>>>>> parent of a4c55b4... javadoc quasi complete + taille de la queue fonctionne + -s et -c fonctionne
     @Override
     public int compareTo(Maillon o) {
         return (this.getValeur().compareTo(o.getValeur()));
--- src/Rules.java
+++ src/Rules.java
@@ -5,25 +5,17 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
 
+<<<<<<< HEAD
 /**
  * <b>Projet.Rules est la classe qui recense les regles du jeu de la vie</b>
  */
+=======
+>>>>>>> parent of a4c55b4... javadoc quasi complete + taille de la queue fonctionne + -s et -c fonctionne
 public class Rules {
     public Liste<Paire> liste;
 
     public String fichier = Principale.f;
 
-    /**
-     * <p>
-     * Cette methode permet la lecture des regles dans un fichier lif.
-     * La ligne commencant par #R est stockée dans un tableau.
-     * Si les regles sont inchangées (#N) le tableau est rempli avec les valeurs [1, 2, /, 3].
-     * </p>
-     *
-     * @return le tableau contenant la ligne des regles.
-     *
-     * @see Rules#fichier
-     */
     public char[] lecture_regles_fichier_lif() {
         char[] r = new char[21];
         String i;
@@ -59,18 +51,6 @@ public class Rules {
         return r;
     }
 
-    /**
-     * Compte le nombre de voisins vivants d'une cellule
-     *
-     * @param l
-     *      La liste des cellules vivantes
-     * @param p
-     *      La paire correspondant aux coordonnées de la cellule dont on compte le nombre de voisins
-     *
-     * @return le nombre de voisins vivants de la cellule
-     *
-     * @see Liste#contains
-     */
     public int NombreVoisin(Liste l, Paire p) {
         int nb = 0;
         Paire voisin1 = new Paire(p.getx() - 1, p.gety() + 1, 0);
@@ -108,25 +88,6 @@ public class Rules {
         return nb;
     }
 
-    /**
-     * <p>
-     *     Renvois la liste des cellules qui naissent.
-     *     Seule les cellules mortes a proximite d'au moins une cellule vivante sont sucseptible de naitre.
-     *     On verifie donc seulement ces cellules, c'est a dire que pour chaque cellule vivante on regarde les caracteristiques de ses voisins.
-     *     Si la cellule verifie un nombre de voisins compris dans le tableau placé en parametres, qu'elle n'appartient pas a la liste des vivantes,
-     *     et qu'elle n'est pas deja dans la liste que l'on renvoie, alors on l'ajoute a cette liste.
-     * </p>
-     *
-     * @param t
-     *      La liste des cellules vivantes
-     * @param tab
-     *      Le tableau contanant le nombre de voisin possible pour qu'une cellule naisse
-     *
-     * @return la liste des cellules qui naissent
-     *
-     * @see Liste#contains
-     * @see Liste#addTrier
-     */
     public Liste mortedevenantvivantes(Liste t, int[] tab) {
         Liste lesvoisinsvivants = new Liste();
         Maillon<Paire> ref = t.tete;
@@ -184,6 +145,7 @@ public class Rules {
         return lesvoisinsvivants;
     }
 
+<<<<<<< HEAD
     /**
      * <p>
      *     Renvois la liste des cellules qui survivent.
@@ -197,6 +159,11 @@ public class Rules {
     /*public Projet.Liste vivantsquirestentvivants(Projet.Liste t, int[] tab) {
         Projet.Liste lesvoisinsvivants = new Projet.Liste();
         Projet.Maillon<Projet.Paire> ref = t.tete;
+=======
+    /*public Liste vivantsquirestentvivants(Liste t, int[] tab) {
+        Liste lesvoisinsvivants = new Liste();
+        Maillon<Paire> ref = t.tete;
+>>>>>>> parent of a4c55b4... javadoc quasi complete + taille de la queue fonctionne + -s et -c fonctionne
         while (ref != null) {
             for (int i = 0; i < tab.length; i++) {
                 Projet.Paire p1 = new Projet.Paire(ref.getValeur().getx() - 1, ref.getValeur().gety() + 1, 0);
@@ -252,22 +219,7 @@ public class Rules {
         return lessurvivants;
     }
 
-    /**
-     * Renvois la liste des cellules a la generation suivante
-     *
-     * @param t
-     *      La liste des cellules a partir de laquelle on va faire une nouvelle generation
-     *
-     * @return la liste des cellules vivantes de la nouvelle generation
-     *
-     * @see Rules#extraire_nb_pour_naitre
-     * @see Rules#extraire_nb_pour_survivre
-     * @see Rules#lecture_regles_fichier_lif
-     * @see Rules#mortedevenantvivantes
-     * @see Rules#vivantsquirestentvivants
-     *
-     */
-     public Liste newgeneration(Liste t) {
+    public Liste newgeneration(Liste t) {
         Liste a = new Liste();
         int[] x = (a.extraire_nb_pour_naitre(a.lecture_regles_fichier_lif()));
         int[] y = (a.extraire_nb_pour_survivre(a.lecture_regles_fichier_lif()));
@@ -275,48 +227,25 @@ public class Rules {
         Liste listevoisinMort = vivantsquirestentvivants(t, y);
         a = listevoisinVivant.concatener((listevoisinMort));
         return a;
-     }
+    }
 
-    /**
-     * Calcule la taille de la queue
-     *
-     * @param t
-     *      La liste dont on cherche la taille de la queue
-     *
-     * @param periode
-     *      La periode de la liste étudiée
-     *
-     * @return la taille de la queue
-     *
-     * @see Rules#newgeneration
-     * @see Liste#identique
-     */
-     public int taille_queue(Liste t, int periode) {
+    public int taille_queue(Liste t, int periode) {
         int taille = 0;
         Liste l = t;
         Liste lp = t;
         while (periode > 0) {
-            lp = newgeneration(lp);
+            lp = lp.newgeneration(lp);
             periode--;
         }
-        while (!l.identique(lp)) {
+        if (!l.identique(lp)) {
             taille++;
-            l = newgeneration(l);
-            lp = newgeneration(lp);
+            l = l.newgeneration(l);
+            lp = lp.newgeneration(lp);
         }
-        return taille-1;
-     }
+        return taille;
+    }
 
-    /**
-     * Retourne le nombre de voisins necessaire pour survivre
-     *
-     * @param l
-     *      Le tableau contenant la ligne des regles du fichier lif (#R)
-     *
-     * @return le nombre de voisins possible pour survivre sous forme de tableau
-     *
-     */
-     public int[] extraire_nb_pour_survivre(char[] l) {
+    public int[] extraire_nb_pour_survivre(char[] l) {
         int taille = 0;
         while (l[taille] != '/') {
             taille++;
@@ -334,18 +263,9 @@ public class Rules {
             p[w] = a[w];
         }
         return p;
-     }
+    }
 
-    /**
-     * Retourne le nombre de voisins necessaire pour qu'une cellule morte naisse
-     *
-     * @param l
-     *      Le tableau contenant la ligne des regles du fichier lif (#R)
-     *
-     * @return le nombre de voisins possible pour naitre sous forme de tableau
-     *
-     */
-     public int[] extraire_nb_pour_naitre(char[] l) {
+    public int[] extraire_nb_pour_naitre(char[] l) {
         int taille_autre = 0;
         while (l[taille_autre] != '/') {
             taille_autre++;
@@ -369,16 +289,9 @@ public class Rules {
             p[b] = a[b];
         }
         return p;
-     }
+    }
 
-    /**
-     *
-     * @param p
-     * @param t
-     * @param tab
-     * @return
-     */
-     public boolean verif_regles(Paire p, Liste t, int[] tab) {
+    public boolean verif_regles(Paire p, Liste t, int[] tab) {
         for (int i = 0; i < tab.length; i++) {
             System.out.println("Nombre voisin : " + NombreVoisin(t, p));
             System.out.println("tab [" + i + "] : " + tab[i]);
@@ -387,64 +300,41 @@ public class Rules {
             }
         }
         return false;
-     }
+    }
 
-    /**
-     * Retourne la periode de la liste en parametre
-     *
-     * @param t
-     *      La liste dont on cherche la periode
-     *
-     * @return la periode de la liste en parametre
-     *
-     * @see Rules#newgeneration
-     * @see Liste#identique
-     */
-     public int periode(Liste t, int max) {
+    public int periode(Liste t) {
         Liste initiale;
         Liste suivante;
         int periode = 0;
+        boolean sameconfig = false;
         initiale = t;
         suivante = newgeneration(t);
         if (initiale.identique(suivante)) {
-            return 1;
+            return periode;
         }
-        while (max != 0) {
+        while (!sameconfig) {
             periode++;
             initiale = newgeneration(initiale);
             suivante = newgeneration(suivante);
             suivante = newgeneration(suivante);
-            max--;
             if (initiale.identique(suivante)) {
-                return periode+1;
+                sameconfig = true;
+                periode++;
             }
         }
-        return 0; //si la methode retourne 0, cela signifie que la periode n'as pas ete trouvé dans le temps max donné
-     }
+        return periode;
+    }
 
-    /**
-     * Retourne le comportement asymptotique de la liste en parametre
-     *
-     * @param t
-     *      La liste étudiée
-     * @param periode
-     *      La periode de la liste étudiée
-     * @param queue
-     *      La taille de la queue de la liste étudiée
-     *
-     * @return le comortement asymptotique de la liste en parametre
-     */
-     public String comportement(Liste t, int periode, int queue) {
-        System.out.print("\n");
+    public String comportement(Liste t, int periode, int queue) {
         if (t == null) {
-            return "Le comportement est : mort\nLa période est de : " + periode + "\nLa taille de la queue est : " + queue + "\n";
+            return "Le comportement est : mort\nLa période est de : " + periode + "\nLa taille de la queue est : " + queue;
         }
         if (periode == 1) {
-            return "Le comportement est : stable\nLa période est de : " + periode + "\nLa taille de la queue est : " + queue + "\n";
+            return "Le comportement est : stable\nLa période est de : " + periode + "\nLa taille de la queue est : " + queue;
         }
         if (periode > 1) {
-            return "Le comportement est : oscillateur\nLa période est de : " + periode + "\nLa taille de la queue est : " + queue + "\n";
+            return "Le comportement est : oscillateur\nLa période est de : " + periode + "\nLa taille de la queue est : " + queue;
         }
         return " ";
-     }
+    }
 }
